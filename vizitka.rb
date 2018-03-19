2d601fdefdb0af2d5ae4929f6153bad14ed8223e
Olga Coskun <ovin999@gmail.com>
2018-03-19 10:52:47 +0300

    Init commit

---
+++ vizitka.rb
@@ -0,0 +1,12 @@
+require 'json'
+
+# Читаем содержимое файла colors.json
+file = File.read("#{__dir__}/vizitka.json", encoding: 'UTF-8')
+
+# Делаем ассоциативный массив из содержимого файла с помощью метода JSON.parse
+# и записываем его в переменную info.
+card = JSON.parse(file)
+
+puts "#{card['my_name']} #{card['my_surname'][0]}. #{card['my_third_name']}"
+puts "#{card['my_cep_telefon']}, #{card['my_email']}"
+puts card['skills']
